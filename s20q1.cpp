6f7be40d9ca60cf9f8f4053064bf0c170e74270f
Onkar Satpute <161943848+onkar-satpute@users.noreply.github.com>
2025-04-05 08:32:40 +0000

    half work 20f

--- s20q1.cpp
+++ s20q1.cpp
@@ -1,3 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int sort(int arr[], int arraylength){
+    for(int i = 0; i<arraylength; i++){
+        for(int j=0; j<arraylength-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main(){
+    int array[]={20,40,10,15,45,35};
+    length = sizeof(array) / sizeof(array[0])
+    int resultArr[] = sort(array, length);
+    cout<<resultArr[];
+    return 0;
+}
