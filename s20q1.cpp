213b68b4c6f7c7d09ec4dd424ec7da9272ed2df0
Onkar Satpute <161943848+onkar-satpute@users.noreply.github.com>
2025-04-05 15:28:41 +0000

    10 m complete

--- s20q1.cpp
+++ s20q1.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int sort(int arr[], int arraylength){
+void sort(int arr[], int arraylength){
     for(int i = 0; i<arraylength; i++){
         for(int j=0; j<arraylength-i-1; j++){
             if(arr[j] > arr[j+1]){
@@ -13,10 +13,30 @@ int sort(int arr[], int arraylength){
     }
 }
 
+void sort(float arr[], int arraylength){
+    for(int i = 0; i<arraylength; i++){
+        for(int j=0; j<arraylength-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                float temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
 int main(){
     int array[]={20,40,10,15,45,35};
-    length = sizeof(array) / sizeof(array[0])
-    int resultArr[] = sort(array, length);
-    cout<<resultArr[];
+    float fArray[] = {20.5, 40.2, 10.1, 15.8, 45.0, 35.6};
+    int length = sizeof(array) / sizeof(array[0]);
+    sort(array, length);
+    cout << "Sorted Array: ";
+    for(int i = 0; i < length; i++) {
+        cout << array[i] << " ";
+    }
+    cout << "\nSorted float Array: ";
+    for(int i = 0; i < length; i++) {
+        cout << fArray[i] << " ";
+    }
     return 0;
 }
